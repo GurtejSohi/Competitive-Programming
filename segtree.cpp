e2325a68db01adbbcb322fc4c58160cfa65cc00b
GurtejSohi <gurtej_sohi@yahoo.com>
2017-12-15 00:21:05 +0530

    Removed some bugs in segTree

--- segtree.cpp
+++ segtree.cpp
@@ -1,6 +1,9 @@
+// reference : http://codeforces.com/blog/entry/15729
+
 #include <bits/stdc++.h>
 using namespace std;
 
+#define mid(x, y) (x+y)/2
 const int maxn=1e5+2;
 int s[4*maxn];
 int a[maxn];
@@ -11,12 +14,12 @@ void build(int l=0, int r=n, int node=1){
 		s[node] = a[l];
 		return;
 	}
-	build(l, (l+r)/2, 2*node);
-	build((l+r)/2), r, 2*node+1);
+	build(l, mid(l, r), 2*node);
+	build(mid(l, r), r, 2*node+1);
 	s[node] = s[2*node] + s[2*node+1];
 }
 
-void modify(int p, int x, int l=0, int r=n, node=1){
+void modify(int p, int x, int l=0, int r=n, int node=1){
 	s[node] += x - a[p];
 	if (r-l==1){
 		// s[node] = x;
@@ -24,14 +27,12 @@ void modify(int p, int x, int l=0, int r=n, node=1){
 		return;
 	}
 	// s[node] += x - a[p];
-	if (p < (l+r)/2){
-		modify(p, x, l, (l+r)/2, 2*node);
-	}
-	else modify(p, x, (l+r)/2, r, 2*node+1);
+	if (p < mid(l, r)) modify(p, x, l, mid(l, r), 2*node);
+	else modify(p, x, mid(l, r), r, 2*node+1);
 }
 
-int sum(int l=0, int r=n, int node=1, int x, int y){
+int sum(int x, int y, int l=0, int r=n, int node=1){
 	if (y <= l || x >= r) return 0;
 	if (x <= l && y >= r) return s[node];
-	return sum(l, (l+r)/2, 2*node, x, y) + sum((l+r)/2, r, 2*node+1, x, y);
+	return sum(x, y, l, mid(l, r), 2*node) + sum(x, y, mid(l, r), r, 2*node+1);
 }
